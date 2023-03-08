116360cd23057932f5835236bbbd6226767b8323
HubertPilniak <126026820+HubertPilniak@users.noreply.github.com>
2023-03-08 11:03:01 +0100

    Add files via upload

---
+++ projekt pierwotny/main.js
@@ -0,0 +1,171 @@
+const scene = new THREE.Scene();
+//scene.background = new THREE.Color( 0x0AC );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 5000 );
+camera.position.y = 320;
+camera.lookAt( scene.position );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( new THREE.Color( 0x000000 ) )
+document.getElementsByTagName('body')[0].appendChild( renderer.domElement );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+
+
+
+//skybox
+
+const skybox = new THREE.CubeTextureLoader().load([
+    
+    "./skybox/bkg1_right.png",
+    "./skybox/bkg1_left.png", 
+    "./skybox/bkg1_top.png",
+    "./skybox/bkg1_bot.png",
+    "./skybox/bkg1_front.png",
+    "./skybox/bkg1_back.png"
+]);
+scene.background = skybox;
+
+//orbitcontrols
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.update();
+
+//zmienne dla raycaster
+const kursor = new THREE.Vector2();
+const ray = new THREE.Raycaster();
+
+
+
+
+
+//zmienne własne określające nazwy obiektów
+mercury.userData.nazwa = 'mercury';
+venus.userData.nazwa = 'venus';
+earth.userData.nazwa = 'earth';
+mars.userData.nazwa = 'mars';
+jupiter.userData.nazwa = 'jupiter';
+saturn.userData.nazwa = 'saturn';
+uran.userData.nazwa = 'uran';
+neptun.userData.nazwa = 'neptun';
+
+mercury.userData.predkosc = 0.05;
+mercury.userData.odl_od_slonca = 35;
+
+sun.userData.nazwa = 'reset';
+earth_m.userData.nazwa = 'reset';
+mars_m1.userData.nazwa = 'reset';
+mars_m2.userData.nazwa = 'reset';
+jup_mc1.userData.nazwa = 'reset';
+pierscien_s.userData.nazwa = 'reset';
+
+//ustawienia cieni
+mercury.receiveShadow = true;
+venus.receiveShadow = true;
+earth.receiveShadow = true;
+mars.receiveShadow = true;
+jupiter.receiveShadow = true;
+saturn.receiveShadow = true;
+uran.receiveShadow = true;
+neptun.receiveShadow = true;
+
+earth_m.receiveShadow = true;
+mars_m1.receiveShadow = true;
+mars_m2.receiveShadow = true;
+jup_mc1.receiveShadow = true;
+pierscien_s.receiveShadow = true;
+
+earth_m.castShadow = true;
+mars_m1.castShadow = true;
+mars_m2.castShadow = true;
+jup_mc1.castShadow = true;
+pierscien_s.castShadow = true;
+
+mercury.castShadow = true;
+venus.castShadow = true;
+earth.castShadow = true;
+mars.castShadow = true;
+jupiter.castShadow = true;
+saturn.castShadow = true;
+uran.castShadow = true;
+
+//dodanie obiektów do sceny
+const group = new THREE.Group();
+group.add( sun_group );
+group.add( earth_group );
+group.add( mercury_group );
+group.add( venus_group );
+group.add( mars_group );
+group.add( jupiter_group );
+group.add( saturn_group );
+group.add( uran_group );
+group.add( neptun_group );
+
+scene.add( group );
+
+
+
+function body_movement(name, speed, center_distance){
+	
+  name.rotation.y += speed;
+  name.position.set( center_distance*Math.cos(name.rotation.y),0,center_distance*Math.sin(name.rotation.y));
+
+}
+
+function point_all_objects(){
+	
+	for(let j = 0; j < 8; j++)
+	{
+		body_movement(name_tab[j], speed_tab[j], sun_distance_tab[j], group_tab[j]);
+	}
+}
+
+// funkcja zapewniająca animaccję układu
+function animate() {
+  controls.update();
+  point_all_objects();
+  
+  requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
+animate();
+
+
+// obsługa klawiatury
+window.addEventListener(
+  'keydown',
+  function( e ) {
+    switch ( e.key ) {
+      case 'w':
+		break;
+		
+	  case 'l':
+			if ( ALight == false ) 
+			{
+				scene.add( ambientLight )
+				ALight = true
+				break;
+			}
+			ALight = false;
+			scene.remove( ambientLight );
+			break;
+      default:
+        ;
+    }   
+    camera.lookAt( scene.position );
+    renderer.render( scene, camera );
+  },
+  false
+);
+
+// zmiana rozmiaru okna
+window.addEventListener(
+  'resize',
+  function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+  },
+  false
+);
