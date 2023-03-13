f119fe9a248aff12f8ef5746caeff5199d2db089
Kamox202 <38615505+Kamox202@users.noreply.github.com>
2023-03-14 00:37:07 +0100

    c

--- projekt pierwotny/main_grav.js
+++ projekt pierwotny/main_grav.js
@@ -1,5 +1,5 @@
 const scene = new THREE.Scene();
-//scene.background = new THREE.Color( 0x0AC );
+scene.background = new THREE.Color( 0x0AC );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.y = 100;
 camera.lookAt( scene.position );
